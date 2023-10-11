b0ced91f06b5ae45021a2eb2414292910b30d834
b3reketab <141371508+b3reketab@users.noreply.github.com>
2023-10-12 01:14:08 +0300

    add a simple ui

--- game.js
+++ game.js
@@ -3,55 +3,66 @@ let getComputerChoice = function() {
     else if(Math.random() >= 0.6) return 'paper';
     else return 'scissors';
 };
-let result = '';
 let playerScore = 0;
 let computerScore = 0;
+const result = document.querySelector('#result');
+const score = document.querySelector('#score');
 function playRound(player, computer) {
     if (player == computer) {
-        result = "It's a tie!";
-        return result + `  player: ${playerScore} - computer: ${computerScore}`;
+        result.textContent = "It's a tie!";
+        score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
     } else if (player == 'rock') {
         if (computer == 'scissors') {
-            result = `${player} beats ${computer}.`;
+            result.textContent = `${player} beats ${computer}.`;
             playerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
         if (computer == 'paper') {
-            result = `${computer} beats ${player}.`;
+            result.textContent = `${computer} beats ${player}.`;
             computerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
     } else if (player == 'paper') {
         if (computer == 'scissors') {
-            result = `${computer} beats ${player}.`;
+            result.textContent = `${computer} beats ${player}.`;
             computerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
         if (computer == 'rock') {
-            result = `${player} beats ${computer}.`;
+            result.textContent = `${player} beats ${computer}.`;
             playerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
     } else if (player == 'scissors') {
         if (computer == 'paper') {
-            result = `${player} beats ${computer}.`;
+            result.textContent = `${player} beats ${computer}.`;
             playerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
         if (computer == 'rock') {
-            result = `${computer} beats ${player}.`;
+            result.textContent = `${computer} beats ${player}.`;
             computerScore++;
-            return result + `  player: ${playerScore} - computer: ${computerScore}`;
+            score.textContent = `  player: ${playerScore} - ${computerScore} :computer`;
         }
-    } else return 'Invalid input!';
+    } else result.textContent = 'Invalid input!';
 }
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        const player = prompt('ENTER YOUR CHOICE: ', );
-        const computer = getComputerChoice();
-        console.log([i] + '. ' + playRound(player, computer));
-    }
-    if (playerScore > computerScore) return console.log(`Congratulations! you win.`);
-        else return console.log(`Computer wins!`);
-}
-game();
+const btnroc = document.querySelector('#btn1');
+const btnpap = document.querySelector('#btn2');
+const btnsciss = document.querySelector('#btn3');
+
+btnroc.addEventListener('click', () => {
+    player = 'rock';
+    let computer = getComputerChoice();
+    playRound(player, computer);
+});
+btnpap.addEventListener('click', () => {
+    player = 'paper';
+    let computer = getComputerChoice();
+    playRound(player, computer);
+});
+btnsciss.addEventListener('click', () => {
+    player = 'scissors';
+    let computer = getComputerChoice();
+    playRound(player, computer);
+});
+
